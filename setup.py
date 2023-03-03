e33dfcd307b35f1e25cc778f0a6274e64e1bac80
KamiSkidder <87556065+KamiSkidder@users.noreply.github.com>
2023-03-03 22:58:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup
 setup(
     name="okome-lib",
     version="1.0",
-    install_requires=["requests"]
+    install_requires=["requests"],  
+    description="m.kuku.lu wrapper",
+    long_description=open("README.md").read(),
 )
